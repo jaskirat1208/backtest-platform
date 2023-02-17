d4639c8d66afe9fb1f3624bf43df008a49c16b61
Jaskirat <jaskiratsingh1208@gmail.com>
2023-02-16 21:42:26 -0400

    Added 5 second sleep with 3 retries

--- alphatools/utils/smartapi_helper.py
+++ alphatools/utils/smartapi_helper.py
@@ -1,3 +1,5 @@
+import time
+
 from smartapi import SmartWebSocket, SmartConnect
 import mintotp
 import requests
@@ -36,14 +38,21 @@ class SmartApiHelper:
     @cache.cache_to_file(CACHE_FILE_PATH, _candle_info_params_hash)
     def get_candle_info(self, candle_info_params):
         candle_info_results = []
-        try:
-            smart_conn = SmartConnect(api_key=self.api_key)
-            totp = mintotp.totp(self.totp_key)
-            smart_conn.generateSession(self.client_code, self.passwd, totp)
-            candle_info_results = smart_conn.getCandleData(candle_info_params)
-            smart_conn.terminateSession(self.client_code)
-        except Exception as e:
-            logging.exception("Historical API failed with exception")
+
+        n_retries_left = 3
+        while n_retries_left > 0:
+            try:
+                smart_conn = SmartConnect(api_key=self.api_key)
+                totp = mintotp.totp(self.totp_key)
+                smart_conn.generateSession(self.client_code, self.passwd, totp)
+                candle_info_results = smart_conn.getCandleData(candle_info_params)
+                smart_conn.terminateSession(self.client_code)
+            except Exception as e:
+                logging.exception("Historical API failed with exception")
+                logging.info("Waiting for 5 seconds.")
+                time.sleep(5)
+                n_retries_left -= 1
+                logging.info("Retrying. {} retries left".format(n_retries_left))
             # self.logger.error()
 
         return candle_info_results
